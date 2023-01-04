d72b62ff9153019a062c98165309fee9908d05be
Елена Калинина <kalinina-elena-i@yandex.ru>
2023-01-04 23:09:35 +0500

    code accorded PEP8

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ from random import randint
 
 def attack(char_name, char_class):
     if char_class == 'warrior':
-        print(f'{char_name} нанёс урон противнику равный'
-              + '{5 + randint(3, 5)}')
-    elif char_class == 'mage':
-        print(f'{char_name} нанёс урон противнику равный'
-              + '{5 + randint(5, 10)}')
-    else:
-        print(f'{char_name} нанёс урон противнику равный'
-              + '{5 + randint(-3, -1)}')
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(3, 5)}')
+    if char_class == 'mage':
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(5, 10)}')
+    if char_class == 'healer':
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(-3, -1)}')
+    return (f'{char_name} не нанес урон противнику')
 
 
 def defence(char_name, char_class):
     if char_class == 'warrior':
-        print(f'{char_name} блокировал {10 + randint(5, 10)} урона')
-    elif char_class == 'mage':
-        print(f'{char_name} блокировал {10 + randint(-2, 2)} урона')
-    else:
-        print(f'{char_name} блокировал {10 + randint(2, 5)} урона')
+        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+    if char_class == 'mage':
+        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+    if char_class == 'healer':
+        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+    return (f'{char_name} не блокировал урон')
 
 
 def special(char_name, char_class):
     if char_class == 'warrior':
-        print(f'{char_name} применил специальное умение «Выносливость'
-              + '{80 + 25}»')
-    elif char_class == 'mage':
-        print(f'{char_name} применил специальное умение «Атака {5 + 40}»')
-    else:
-        print(f'{char_name} применил специальное умение «Защита {10 + 30}»')
+        return (f'{char_name} применил специальное умение «Выносливость'
+                f'{80 + 25}»')
+    if char_class == 'mage':
+        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+    if char_class == 'healer':
+        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return (f'{char_name} не применил специальное умение')
 
 
 def start_training(char_name, char_class):
@@ -41,8 +44,8 @@ def start_training(char_name, char_class):
         print(f'{char_name}, ты Лекарь — чародей, способный исцелять раны.')
     print('Потренируйся управлять своими навыками.')
     print('Введи одну из команд: attack — чтобы атаковать противника,'
-          + 'defence - чтобы блокировать атаку противника или special — чтобы'
-          + 'использовать свою суперсилу.')
+          'defence - чтобы блокировать атаку противника или special — чтобы'
+          'использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd = None
     while cmd != 'skip':
@@ -61,20 +64,20 @@ def choice_char_class():
     char_class = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь'
-                           + 'играть: Воитель — warrior, Маг — mage,'
-                           + 'Лекарь — healer: ')
+                           'играть: Воитель — warrior, Маг — mage,'
+                           'Лекарь — healer: ')
         if char_class == 'warrior':
             print('Воитель — дерзкий воин ближнего боя.'
-                  + 'Сильный, выносливый и отважный.')
+                  'Сильный, выносливый и отважный.')
         if char_class == 'mage':
             print('Маг — находчивый воин дальнего боя.'
-                  + 'Обладает высоким интеллектом.')
+                  'Обладает высоким интеллектом.')
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель.'
-                  + 'Черпает силы из природы, веры и духов.')
+                  'Черпает силы из природы, веры и духов.')
         approve_choice = input('Нажми (Y), чтобы подтвердить выбор,'
-                               + 'или любую другую кнопку, чтобы выбрать'
-                               + 'другого персонажа ').lower()
+                               'или любую другую кнопку, чтобы выбрать'
+                               'другого персонажа ').lower()
     return char_class
 
 
